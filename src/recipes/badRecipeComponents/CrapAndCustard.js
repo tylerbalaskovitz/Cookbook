834ef5059903989e6a072006ad124df5c98c0c1f
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-12-21 23:33:36 -0500

    Continuing to add the components so they connect with each other, and using useState as a way to take the data from the json file and pass it into the components so it will take the entire list of data

--- src/recipes/badRecipeComponents/CrapAndCustard.js
+++ src/recipes/badRecipeComponents/CrapAndCustard.js
@@ -5,17 +5,28 @@ import CrapAndCustardData from './CrapAndCustardData.json'
 import TaskMapper from '../../taskMapper.js'
 
 function CrapAndCustard({crapAndCustard}) {
+const [crapAndCustardData, setCrapAndCustardData] = useState(CrapAndCustardData)
+const [renderRecipe, setRenderRecipe] = useState(false);
+
+function renderRecipe() {
+	if (renderRecipe){
+		setRenderRecipe(false)
+	} else {
+		setRenderRecipe(true)
+	}
+}
+
   return (
   	<header>
 	CrapAndCustard
 	</header>
     <div className="CookingSteps">
 	Crap and Custard is an old time classic that is shared with the friends and family through generations of poverty, despair, and comedy. So, have a laugh and share with your unwitting friends, inlaws and strangers
-	<div>
-	Shit sandwich
-	</div>
 	<div className="PictureOfRecipe">
 	</div>
+	<button className=recipeButton onClick={renderRecipe}> 
+		See Recipe 
+	</button>
     </div>
   );
 }
