c04e271e54393ef3f5c6c182d931e4b550e8be35
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-12-22 00:11:58 -0500

    trying to pass the list of values into the program and then pass it to their subcomponents

--- src/recipes/BadRecipes.js
+++ src/recipes/BadRecipes.js
@@ -4,8 +4,13 @@ import DiarheaDejour from './badRecipeComponents/DiarheaDejour.js
 import shitSandwichfrom './badRecipeComponents/shitSandwich.js
 import CrapAndCustard from './badRecipeComponents/CrapAndCustard.js
 import React, {useState} from 'react';
+import CrapAndCustardList from './badRecipeComponents/CrapAndCustardData.json'
 
 function BadRecipes() {
+//here there will need to be four different use states, one for each of the recipes
+// that are being passed in.
+const [crapAndCustardList, setCrapAndCustardList] = useState(CrapAndCustardData)
+
   return (
     <div className="GoodRecipes">
       <div className="GoodRecipes-header">These are the Bad Recipes. Take a Pick
@@ -14,7 +19,7 @@ function BadRecipes() {
 	Shit sandwich
 	</div>
 	<div>
-		<CrapAndCustard />
+		<CrapAndCustard crapAndCustardList={crapAndCustardList} />
 	</div>
 	<div>
 	Diarhea De Jour
