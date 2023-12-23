df256890b773c7ab0ba45225bd6d1737496f50db
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-12-22 22:52:15 -0500

    Making changes to CrapAndCustard file to try to debug and find out what is wrong with the code.

--- src/recipes/badRecipeComponents/CrapAndCustard.js
+++ src/recipes/badRecipeComponents/CrapAndCustard.js
@@ -1,14 +1,12 @@
 import './GoodRecipes.css';
 import React, {useState} from 'react';
 import CrapAndCustardImage from '../../images/crapandcustard.jpg'
-import crapAndCustardData from './CrapAndCustardData.json'
 import TaskMapper from '../../taskMapper.js'
 
-function CrapAndCustard({crapAndCustardList}) => {
-const [crapAndCustardData, setCrapAndCustardData] = useState(CrapAndCustardData)
+function CrapAndCustard ({crapAndCustardList}) {
 const [renderRecipe, setRenderRecipe] = useState(false);
 
-function renderRecipe() {
+function renderRecipeButton() {
 	if (renderRecipe){
 		setRenderRecipe(false)
 	} else {
@@ -17,21 +15,25 @@ function renderRecipe() {
 }
 
   return (
+	<div>
   	<header>
 	CrapAndCustard
 	</header>
     <div className="CookingSteps">
 	Crap and Custard is an old time classic that is shared with the friends and family through generations of poverty, despair, and comedy. So, have a laugh and share with your unwitting friends, inlaws and strangers
+	</div>
 	<div className="PictureOfRecipe">
 		{CrapAndCustardImage}
 	</div>
-	<button className=recipeButton onClick={renderRecipe}> 
-		See Recipe 
-	</button>
-    	<div>
-		{renderRecipe ? {crapAndCustardList.map(todo => {
-			return (<TaskMapper taskmapper={taskmapper}/>) : null })}}
-	</div>
+		<div>
+		<button className=recipeButton onClick={renderRecipeButton}> 
+			See Recipe 
+		</button>
+		</div>
+		<div>
+			{renderRecipe ? {crapAndCustardList.map(todo => {
+				return (<TaskMapper taskmapper={taskmapper}/>) : null })}}
+		</div>
     </div>
   );
 }
