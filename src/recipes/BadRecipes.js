f48d7a5725af9c2d27f50a1fee5a07362efc8672
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-12-25 22:43:12 -0500

    Added another recipe the Shit Sandwich to the webpage

--- src/recipes/BadRecipes.js
+++ src/recipes/BadRecipes.js
@@ -1,24 +1,26 @@
 import './GoodRecipes.css';
 import CrapfilledCreamSoup from './badRecipeComponents/CrapfilledCreamSoup.js';
 import DiarheaDejour from './badRecipeComponents/DiarheaDeJour.js';
-import shitSandwich from './badRecipeComponents/shitSandwich.js';
+import ShitSandwich from './badRecipeComponents/shitSandwich.js';
 import CrapAndCustard from './badRecipeComponents/CrapAndCustard.js';
 import React, {useState} from 'react';
 import CrapAndCustardList from './badRecipeComponents/CrapAndCustardData.json'
 import DiarheaDeJourList from './badRecipeComponents/DiarheaDeJourData.json'
+import ShitSandwichList from './badRecipeComponents/ShitSandwichData.json'
 
 function BadRecipes() {
 //here there will need to be four different use states, one for each of the recipes
 // that are being passed in.
 const [crapAndCustardList, setCrapAndCustardList] = useState(CrapAndCustardList)
 const [diarheaDeJourList, setDiarheaDeJourList] = useState(DiarheaDeJourList)
+const [shitSandwichList, setShitSandwichList] = useState(ShitSandwichList)
 
   return (
     <div className="GoodRecipes">
       <div className="GoodRecipes-header">These are the Bad Recipes. Take a Pick
        Welcome to the recipes section where things get awfully delicious 
 	<div>
-	Shit sandwich
+		<ShitSandwich shitSandwichList={shitSandwichList} />
 	</div>
 	<div>
 		<CrapAndCustard crapAndCustardList={crapAndCustardList} />
--- src/recipes/badRecipeComponents/shitSandwich.js
+++ src/recipes/badRecipeComponents/shitSandwich.js
@@ -1,17 +1,49 @@
 import './GoodRecipes.css';
 import React, {useState} from 'react';
+import ShitSandwichImage from '../../images/shit-sandwich.png'
+import TaskMapper from '../../taskMapper.js'
+import ShitSandwichList from './ShitSandwichData.json'
 
-function ShitSandwich() {
-  return (
-    <div className="GoodRecipes">
-      <div className="GoodRecipes-header">These are the Good Recipes. Take a Pick
-       Welcome to the recipes section where things get awfully delicious 
+function ShitSandwich({shitSandwichList}) {
+  const [renderRecipe, setRenderRecipe] = useState(false);
+
+function renderRecipeButton() {
+	if (renderRecipe){
+		setRenderRecipe(false)
+	} else {
+		setRenderRecipe(true)
+	}
+}
+
+
+return (
 	<div>
-	Shit sandwich
-	</div>
-	</div>
+  	<header>
+	Shit Sandwich
+	{!renderRecipe && 
+		<button className="recipeButton" onClick={renderRecipeButton}> 
+			See Recipe
+		</button>
+	}
+	</header>
+	{renderRecipe && 
+	    <div>
+	    <div className="CookingSteps">
+		From rockstars to politicians, even album reviews, everyone knows loves a shit sandwich from time to time. When life is getting glub and you want to chow down on an ole time favorite that even the movie stars adore (See Pink Flamingos). You'll be presently surprised to find a way to make it all come together with a stinky shit sandwich	
+		</div>
+		<div className="PictureOfRecipe">
+		<img src={ShitSandwichImage} className="FoodImage" alt="food" />
+		</div>
+		<li>
+		  {shitSandwichList.map(ShitSandwichList => {
+			return (<TaskMapper taskMapper={ShitSandwichList}/>)})} 
+		</li>
+		<button className="recipeButton" onClick={renderRecipeButton}> 
+			{renderRecipe ? "Hide Recipe" : "See Recipe"}
+		</button>
+		</div>
+		}
     </div>
   );
 }
-
 export default ShitSandwich;
