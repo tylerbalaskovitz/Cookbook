ca46d8538c6b3134ec2dbd9a91999c671a8206cf
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-12-26 22:13:17 -0500

    Modifying JSON data so the Pizza, and Jiaozi recipes come out nicely

--- src/recipes/goodRecipeComponents/Jiaozi.js
+++ src/recipes/goodRecipeComponents/Jiaozi.js
@@ -28,7 +28,33 @@ function renderRecipeButton() {
 	{renderRecipe && 
 	    <div>
 	    <div className="CookingSteps">
-		Insert description of Jiaozi	
+		    8 ounces
+
+    Napa cabbage (about 1/4 head), plus extra leaves for lining if steaming
+    8 ounces
+
+    ground pork
+    1/2 bunch
+
+    scallions (green and white parts), finely chopped
+    1/2 cup
+
+    finely chopped garlic chives
+    1
+
+    clove garlic, finely chopped
+    2 teapoons
+
+    freshly grated peeled ginger
+    2 tablespoons
+
+    soy sauce
+    1/2 teaspoon
+
+    toasted (Asian) sesame oil
+    1 pound
+
+    round wheat dumpling or potsticker wrappers
 		</div>
 		<div className="PictureOfRecipe">
 		<img src={JiaoziImage} className="FoodImage" alt="food" />
