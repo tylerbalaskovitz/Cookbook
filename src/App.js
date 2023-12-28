55d2f0c49887abfa62f326aa773836131f39783c
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-12-28 00:18:59 -0500

    Making sure to have a Context Provider and Context consumer in the right places when creating the parent component so data can be passed throughout the application

--- src/App.js
+++ src/App.js
@@ -1,33 +1,26 @@
 import logo from './Cookbook.gif'
 import './App.css';
 import {Link, BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
-import {createContext} from 'react';
+import {useState, createContext} from 'react';
 import GoodRecipes from './recipes/GoodRecipes';
 import ImportedButton from './buttons/buttons.js';
 import Header from './header.js';
 import Body from './body.js';
 import Footer from './footer.js';
 //Uncomment out when finishing the other components food, and what not.
-export const RecipeContext = createContext();
+export const RecipeContext = createContext("");
 
-const RecipeContextProvider = ({children}) => {
-const [recipes, setRecipes] = useState(undefined);
-
-return (<RecipeContext.Provider value = {{recipes, setRecipes}}>
-		{children}
-	<Recipecontext.Provicer />
-	);
-};
-
-//Used to render the three main components of the application
 function App() {
+  const [recipes, setRecipes] = useState("");
+
   return (
+
     <div className="App">
-	<RecipeContextProvider>
+	
 	<Header />
 	<Body />
 	<Footer />
-	<RecipeContextProvider/>
+	
     </div>
   );
 }
