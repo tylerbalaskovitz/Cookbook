9221d2dcca3c0c68ff9a4eba336fa7f2ac7c9d40
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-12-28 00:44:52 -0500

    Tested and ensured that the value in the header is being properly used to render the page

--- src/header.js
+++ src/header.js
@@ -23,6 +23,9 @@ function Header() {
 		<button className= "NavigationButton" onClick={badRecipes}> View the shitty stuff </button>
 		<button className= "NavigationButton" onClick={spicyRecipes}>View the Spicy Stuff </button>
 	</p>
+	<div>
+	Recipe for switch {recipes}	
+	</div>
 	</div>
   );
 }
