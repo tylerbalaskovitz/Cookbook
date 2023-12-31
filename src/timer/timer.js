edabbba4bc03d5d19ef15399e8b5472e0941f02b
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-12-31 15:07:00 -0500

    Adding different values for each type of time measurement for example seconds, minutes, hours etc

--- src/timer/timer.js
+++ src/timer/timer.js
@@ -6,6 +6,10 @@ import CrapAndCustardList from './CrapAndCustardData.json'
 
 function Timer () {
 const [timerOn, setTimerOn] = useState(false);
+const [seconds, setSeconds] = useState(0);
+const [minutes, setMinutes] = useState(0);
+const [hours, setHours] = useState(0);
+const [days, setDays] = useState(0);
 
 function turnOnTimer(){	
 	(timerOn ? setTimerOn(false) : setTimerOn(true));
