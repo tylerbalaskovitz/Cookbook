1f6e30fd740c02b3070e34eb5880c039ecc48597
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2023-12-31 15:12:46 -0500

    creating a method that sets the interval of the timer so it will count in seconds

--- src/timer/timer.js
+++ src/timer/timer.js
@@ -1,8 +1,8 @@
 import './GoodRecipes.css';
-import React, {useState} from 'react';
-import CrapAndCustardImage from '../../images/crapandcustard.jpg'
-import TaskMapper from '../../taskMapper.js'
-import CrapAndCustardList from './CrapAndCustardData.json'
+import React, {useState, useEffect} from 'react';
+
+
+
 
 function Timer () {
 const [timerOn, setTimerOn] = useState(false);
@@ -10,6 +10,12 @@ const [seconds, setSeconds] = useState(0);
 const [minutes, setMinutes] = useState(0);
 const [hours, setHours] = useState(0);
 const [days, setDays] = useState(0);
+
+useEffect(() => {
+const interval = setInterval(() => getTime, 1000);
+
+return (() => clearInterval(interval);
+}, []);
 
 function turnOnTimer(){	
 	(timerOn ? setTimerOn(false) : setTimerOn(true));
