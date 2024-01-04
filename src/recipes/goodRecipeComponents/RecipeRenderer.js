98ada8ce1f00a9381f598911868fa2ed5ecd5e59
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2024-01-04 16:29:38 -0500

    Got the recipe renderer component created

---
+++ src/recipes/goodRecipeComponents/RecipeRenderer.js
@@ -0,0 +1,56 @@
+import '../RecipeStyling.css';
+import React, {useState} from 'react';
+import RecipeImage from '../../images/beefmeatballs.jpg'
+import TaskMapper from '../../taskMapper.js'
+import IngredientMapper from '../../IngredientMapper.js'
+import BeefMeatballsList from './BeefMeatballsData.json'
+import Ingredients from './BeefMeetballsIngredients.json'
+
+function BeefMeatballs ({beefMeatballsList, ingredientsList}) {
+const [renderRecipe, setRenderRecipe] = useState(false);
+
+function renderRecipeButton() {
+	if (renderRecipe){
+		setRenderRecipe(false)
+	} else {
+		setRenderRecipe(true)
+	}
+}
+
+  return (
+	<div>
+  	<header>
+	Beef Meatballs	
+	{!renderRecipe && 
+		<button className="recipeButton" onClick={renderRecipeButton}> 
+			See Recipe
+		</button>
+	}
+	</header>
+	{renderRecipe && 
+	    <div>
+		<div className="PictureOfRecipe">
+		<img src={RecipeImage} className="FoodImage" alt="food" />
+		</div>
+	    <div> Ingredients </div>
+	    <div className="CookingSteps">
+		  {ingredientsList.map(Ingredients => {
+			return (<IngredientMapper mapper = {Ingredients}/>)})} 
+		</div>
+		<div>
+		How to Cook
+		</div>
+		<ol className="CookingSteps">
+		  {beefMeatballsList.map(BeefMeatballsList => {
+			return (<li><TaskMapper taskMapper={BeefMeatballsList}/></li>)})} 
+		</ol>
+		<button className="recipeButton" onClick={renderRecipeButton}> 
+			{renderRecipe ? "Hide Recipe" : "See Recipe"}
+		</button>
+		</div>
+		}
+    </div>
+  );
+}
+
+export default BeefMeatballs;
