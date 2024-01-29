52769ff4bfe8b4c911bf027bde6749046b8861a7
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2024-01-28 19:51:08 -0500

    Added the ability to POST data in the Cookbook on the backend so data will go to the user section of the database

--- node/user/UserController.js
+++ node/user/UserController.js
@@ -1,1 +1,25 @@
 //User Controller File
+var express = require('express');
+var router = express.Router();
+var bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({extended: true })); 
+router.use(bodyParser.json());
+var User = require('./User');
+module.expors = router;
+
+//Creating a new user
+router.post('/', function(req, res) {
+	User.create({
+		name: req.body.name,
+		email: req.body.email,
+		password: req.body.password
+		},
+		function (err, user) {
+			if (err) return res.status(500).send("There was a problem adding information to the database.");
+				res.status(200).send(user);
+		});
+});
+
+
+
+
