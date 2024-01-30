194cd562fa9fadf1e232a5c07d596106da2c94bd
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2024-01-30 18:51:58 -0500

    Quick Push

--- node/user/UserController.js
+++ node/user/UserController.js
@@ -7,19 +7,18 @@ router.use(bodyParser.json());
 var User = require('./User');
 
 //Creating a new user
-router.post('/', async (req, res) =>  {
-		const postRequest = await function User.create(req, res) {
+router.post('/', async function (req, res) {
 		try {
 		const user = await User.create(req.body);
 		res.status(200).send({
 			name: req.body.name,
 			email: req.body.email,
-			password: req.body.password
+			password: req.body.password,
 		});
 		}catch(err) {
 			 res.status(500).send("There was a problem adding information to the database." + err);
 		};
-}});
+});
 
 //Performs a GET Request to get all of the users in the database
 router.get('/', async function (req, res) {
