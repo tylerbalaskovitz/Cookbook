83f4077d4b00cbae31e1a9cfb45eb36fc808e4e4
Tyler Balaskovitz <tylerbalaskovitz@yahoo.com>
2024-01-30 18:47:14 -0500

    Still working on Asynchronous functions to launch the server

--- node/user/UserController.js
+++ node/user/UserController.js
@@ -7,26 +7,30 @@ router.use(bodyParser.json());
 var User = require('./User');
 
 //Creating a new user
-router.post('/', function(req, res) {
-	User.create(req).then((res) => {
-		res.send({
-		name: req.body.name,
-		email: req.body.email,
-		password: req.body.password
-		})
-		})
-		.catch((err) => {
-			if (err) return res.status(500).send("There was a problem adding information to the database." + err);
-				res.status(200).send(user);
+router.post('/', async (req, res) =>  {
+		const postRequest = await function User.create(req, res) {
+		try {
+		const user = await User.create(req.body);
+		res.status(200).send({
+			name: req.body.name,
+			email: req.body.email,
+			password: req.body.password
 		});
-});
+		}catch(err) {
+			 res.status(500).send("There was a problem adding information to the database." + err);
+		};
+}});
 
 //Performs a GET Request to get all of the users in the database
-router.get('/', function (req, res) {
-	 User.find({}, function (err, users) {
-		if (err) return res.status(500).send("There was a problem finding the users.");
-		res.status(200).send(users);
-		});
+router.get('/', async function (req, res) {
+		try { 
+		const users = await User.find({});
+			res.status(200).send(users);
+			console.log(users);
+		} catch (err) {
+			res.status(500).send("There was a problem getting the users");
+			console.log(err);
+		}
 });
 
 module.exports = router;
